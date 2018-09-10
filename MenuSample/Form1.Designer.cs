e51e2e469cf84a07e8fa540f93816e3c549ad2e2
in_dow <in_dow@hotmail.com>
2018-09-11 03:51:06 +0900

    Add more samples

--- MenuSample/Form1.Designer.cs
+++ MenuSample/Form1.Designer.cs
@@ -28,26 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.textLog = new System.Windows.Forms.TextBox();
             this.listMenu1 = new MenuSample.ListMenu();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.listMenu1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textLog, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 45.55556F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 54.44444F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // textLog
+            // 
+            this.textLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textLog.Location = new System.Drawing.Point(403, 208);
+            this.textLog.MaxLength = 0;
+            this.textLog.Multiline = true;
+            this.textLog.Name = "textLog";
+            this.textLog.ReadOnly = true;
+            this.textLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textLog.Size = new System.Drawing.Size(394, 239);
+            this.textLog.TabIndex = 1;
+            this.textLog.WordWrap = false;
             // 
             // listMenu1
             // 
             this.listMenu1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listMenu1.ItemHeight = 50;
-            this.listMenu1.Location = new System.Drawing.Point(0, 0);
+            this.listMenu1.Location = new System.Drawing.Point(3, 3);
             this.listMenu1.Name = "listMenu1";
-            this.listMenu1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.SetRowSpan(this.listMenu1, 2);
+            this.listMenu1.Size = new System.Drawing.Size(394, 444);
             this.listMenu1.TabIndex = 0;
+            this.listMenu1.MenuGroupSelectionChanged += new System.EventHandler<MenuSample.MenuEventArgs>(this.listMenu1_MenuGroupSelectionChanged);
+            this.listMenu1.MenuDetailSelectionChanged += new System.EventHandler<MenuSample.MenuEventArgs>(this.listMenu1_MenuDetailSelectionChanged);
+            this.listMenu1.MenuDetailDoubleClick += new System.EventHandler<MenuSample.MenuEventArgs>(this.listMenu1_MenuDetailDoubleClick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.listMenu1);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -55,6 +92,8 @@
         #endregion
 
         private ListMenu listMenu1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox textLog;
     }
 }
 
--- MenuSample/Form1.cs
+++ MenuSample/Form1.cs
@@ -21,7 +21,16 @@ namespace MenuSample
             var group2 = new ListViewGroup("group2");
             listMenu1.Groups.Add(group2);
             listMenu1.Items.Add(new MenuListViewItem(new[] { "aaaaa01", "subtitle" }, group1));
-            listMenu1.Items.Add(new MenuListViewItem(new[] { "bbbbb02", "subtitle" }, group1).SubMenuAdd(new MenuListViewItem(new[] { "bbbbb05", "subtitle" }), new MenuListViewItem(new[] { "bbbbb06", "subtitle" })));
+
+            var subgroup1 = new ListViewGroup("sub group1");
+            var subgroup2 = new ListViewGroup("sub group2");
+            listMenu1.Items.Add(new MenuListViewItem(new[] { "bbbbb02", "subtitle" }, group1)
+                .SubMenuAdd(
+                    new MenuListViewItem(new[] { "bbbbb05", "subtitle" }, subgroup1)
+                    , new MenuListViewItem(new[] { "bbbbb06", "subtitle" }, subgroup1)
+                    , new MenuListViewItem(new[] { "bbbbb07", "subtitle" }, subgroup2)
+                    )
+                .SubMenuAdd(subgroup1, subgroup2));
             listMenu1.Items.Add(new MenuListViewItem(new[] { "aaaaa03", "subtitle" }, group1).SubMenuAdd(new MenuListViewItem(new[] { "bbbbb05", "subtitle" })));
             listMenu1.Items.Add(new MenuListViewItem(new[] { "bbbbb04", "subtitle" }, group1));
             listMenu1.Items.Add(new MenuListViewItem(new[] { "aaaaa05", "subtitle" }, group2));
@@ -31,6 +40,21 @@ namespace MenuSample
             listMenu1.Items.Add(new MenuListViewItem(new[] { "aaaaa09", "subtitle" }, group2));
             listMenu1.Items.Add(new MenuListViewItem(new[] { "bbbbb10", "subtitle" }, group2));
             (listMenu1.Items[0] as MenuListViewItem).Enabled = false;
+        }
+
+        private void listMenu1_MenuGroupSelectionChanged(object sender, MenuEventArgs e)
+        {
+            textLog.AppendText(string.Format("Group selected: {0}\r\n", e.Item.Text));
+        }
+
+        private void listMenu1_MenuDetailSelectionChanged(object sender, MenuEventArgs e)
+        {
+            textLog.AppendText(string.Format("Detail selected: {0}\r\n", e.Item.Text));
+        }
+
+        private void listMenu1_MenuDetailDoubleClick(object sender, MenuEventArgs e)
+        {
+            textLog.AppendText(string.Format("Detail double clicked: {0}\r\n", e.Item.Text));
         }
     }
 }
